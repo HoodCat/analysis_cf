e0eb5f09e4bb5db7474490af909a13f3304da264
HoodCat <thanatos0128@gmail.com>
2018-05-31 10:05:40 +0900

    init commit

---
+++ __test__/test_bs4.py
@@ -0,0 +1,15 @@
+# Beautifulsoup4 test
+from bs4 import BeautifulSoup
+
+
+html = '<td class="title"> ' \
+            '<div class="tit3">' \
+                '<a href="/movie/bi/mi/basic.nhn?code=158178" title="독전">독전</a>' \
+            '</div>' \
+       '</td>'
+
+# 1. 조회
+
+# 2. Attribute 값
+
+# 3. Attribute 조회
